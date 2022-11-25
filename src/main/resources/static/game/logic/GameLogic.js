ff9c43ee2c8bb061926f1691b7654f26e2e6705d
JeffreyHSE <Jeffrey.Ejeanye@hoffmann-group.com>
2022-11-25 10:39:20 +0100

    added randomizeArray so it randomizes the computer_choices. Also, successfully created computerChoice methode(works).

--- src/main/resources/static/game/logic/GameLogic.js
+++ src/main/resources/static/game/logic/GameLogic.js
@@ -1,4 +1,8 @@
+import {userInput} from "../events/click.js";
+
 export class GameLogic {
+
+    computer_choice_array = ["✂️", "🪨", "📝"];
 
     constructor() {
 
@@ -6,7 +10,33 @@ export class GameLogic {
 
     userChoice() {
         let get_user_choice = document.getElementById("user_choice");
-        console.log(get_user_choice.innerHTML);
+        let update_user_choice_frontend = get_user_choice.innerHTML.replace(get_user_choice.innerHTML[6], userInput);
+        let update_data = document.createElement("p");
+        update_data.setAttribute("id", "user_choice");
+        update_data.innerHTML = update_user_choice_frontend;
+
+        get_user_choice.replaceWith(update_data);
+
+        console.log(update_user_choice_frontend);
     }
+
+    randomizeArray() {
+        this.computer_choice_array.sort((a, b) => 0.5 - Math.random());
+    }
+
+    computerChoice() {
+        this.randomizeArray();
+        console.log(this.computer_choice_array[0]);
+        let get_computer_choice = document.getElementById("computer_choice");
+        let update_computer_choice_frontend = get_computer_choice.innerHTML.replace(get_computer_choice.innerHTML[10], this.computer_choice_array[0]);
+        let update_data = document.createElement("p");
+        update_data.setAttribute("id", "computer_choice");
+        update_data.innerHTML = update_computer_choice_frontend;
+
+        get_computer_choice.replaceWith(update_data);
+
+        console.log(update_computer_choice_frontend);
+    }
+
 
 }
